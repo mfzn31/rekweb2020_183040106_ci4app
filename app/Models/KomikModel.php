2a00e81fd8705ba68964acbf35d6496215b22976
mfzn31 <fauzan.183040106@mail.unpas.ac.id>
2020-10-27 17:31:10 +0700

    pertama commit

---
+++ app/Models/KomikModel.php
@@ -0,0 +1,9 @@
+<?php
+
+use CodeIgniter\Model;
+
+class KomikModel extends Model
+{
+    protected $table      = 'komik';
+    protected $useSoftDeletes = true;
+}
---
+++ app/Views/pages/about.php
@@ -0,0 +1,16 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Praesentium, placeat accusantium dolores at, neque, veniam doloremque dignissimos corporis error rerum quas tempore.
+                Cumque doloremque suscipit et sunt autem sint sapiente?</p>
+        </div>
+    </div>
+</div>
+
+<?= $this->endSection(); ?>
